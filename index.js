4152b66debed784d075f546462ad1ec14594a8b9
Arnaud Didry <arnaud@didry.info>
2015-12-09 17:45:53 +0100

    Add KUE_UI_PATH environment variable

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
 var kue = require('kue');
+var express = require('express');
 
 kue.createQueue({
     redis: process.env['REDIS_URL']
 });
 
-kue.app.listen(process.env['WEB_PORT'] || 3000);
+var app = express();
+
+app.use('/' + process.env['KUE_UI_PATH'], kue.app);
+
+var server = app.listen(process.env.NODE_PORT);
